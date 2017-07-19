ce38c3167cee0e656008afe026eecf8acd7e63b1
luoyin <luoyin1994@icloud.com>
2017-07-20 00:30:54 +0800

    add function {_toDefault,_baseEmpty2string,_cleanArr,_cleanObj,_cleanBoth}

--- js/main.js
+++ js/main.js
@@ -1,6 +1,112 @@
 'use strict';
-exports._typeof = type => {
+/**
+ * 类型检测
+ * @param type
+ * @returns {*}
+ * @private
+ */
+let _typeof = type => {
     let toString = Object.prototype.toString;
     let string = toString.call(type).match(/^\[object\s(\w*)\]$/)[1];
     return string.toLowerCase();
+};
+
+/**
+ * 置为默认值
+ * 传入各个类型的值，并将其值置为相应类型的指定的默认值
+ * @param value
+ * @returns {*}
+ * @private
+ */
+let _toDefault = value => {
+    // 可以从配置文件导入，实现自定义配置
+    let types = [
+        {name:'Boolean',value:false},
+        {name:'Number',value:-1},
+        {name:'String',value:''},
+        {name:'Function',value:new Function},
+        {name:'Array',value:[]},
+        {name:'Date',value:new Date},
+        {name:'RegExp',value:new RegExp},
+        {name:'Object',value:{}},
+        {name:'Undefined',value:''},
+        {name:'Null',value:''},
+        {name:'Error',value:new Error},
+    ];
+    types.forEach(function(type,i){
+        if(_typeof(value) == type.name.toLowerCase()){
+            value = type.value;
+        }
+    });
+    return value;
 }
+
+/**
+ * 转化【'',null,undefined】为【''】
+ * @param value
+ * @returns {*}
+ * @private
+ */
+let _baseEmpty2string = value => {
+     let types = ['',null,undefined];
+     types.forEach(function (item) {
+         if(value === item) {
+             value = '';
+         }
+     })
+    return value;
+};
+
+/**
+ * 清理数组中不干净的数据，
+ * 包括【null】
+ * @param arr
+ * @returns {*}
+ * @private
+ */
+let _cleanArr = arr => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == null) {
+            arr.splice(i, 1);
+            i--;
+        }else if(_typeof(arr[i]) == 'array'){
+            _cleanArr(arr[i]);
+        }else if(_typeof(arr[i]) == 'object'){
+            _cleanObj(arr[i]);
+        }
+    }
+    return arr;
+}
+/**
+ * 清理对象中不干净的数据，
+ * 包括【null】
+ * @param obj
+ * @returns {*}
+ * @private
+ */
+let _cleanObj = obj => {
+    for(let key in obj){
+        if(_typeof(obj[key]) == 'array'){
+            obj[key] =  _cleanArr(obj[key]);
+        }else if(_typeof(obj[key]) == 'object'){
+            _cleanObj(obj[key]);
+        }
+    }
+    return obj;
+}
+
+/**
+ * 清理数组和对象中不干净的数据，
+ * 包括【null】
+ * @param data
+ * @returns {*}
+ * @private
+ */
+let _cleanData = data => {
+    if(_typeof(data) == 'array') return _cleanArr(data);
+    if(_typeof(data) == 'object') return _cleanObj(data);
+}
+module.exports =  {
+    _typeof,_toDefault,_baseEmpty2string,_cleanArr,_cleanObj,_cleanData
+}
+
--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,7 @@
 var main   = require('../js/main')
 var expect = require('assert').equal
-describe('测试"js/main.js"中_typeof函数', function () {
+// var expect = require('chai').expect
+describe('测试"js/main.js"中 _typeofy 函数', function () {
     var test = main._typeof
     it('Number', function () {
         expect('number', test(new Number))
@@ -12,60 +13,110 @@ describe('测试"js/main.js"中_typeof函数', function () {
         expect('number', test(120))
         expect('number', test(012))
     })
-    it('String',function () {
-        expect('string',test(new String))
-        expect('string',test(new String()))
-        expect('string',test(''))
-        expect('string',test('xxx'))
-        expect('string',test('122456'))
-        expect('string',test('122xxx'))
-        expect('string',test('xxx123456'))
+    it('String', function () {
+        expect('string', test(new String))
+        expect('string', test(new String()))
+        expect('string', test(''))
+        expect('string', test('xxx'))
+        expect('string', test('122456'))
+        expect('string', test('122xxx'))
+        expect('string', test('xxx123456'))
     })
-    it('Boolean',function () {
-        expect('boolean',test(new Boolean))
-        expect('boolean',test(new Boolean()))
-        expect('boolean',test(false))
-        expect('boolean',test(true))
+    it('Boolean', function () {
+        expect('boolean', test(new Boolean))
+        expect('boolean', test(new Boolean()))
+        expect('boolean', test(false))
+        expect('boolean', test(true))
     })
-    it('Function',function () {
-        expect('function',test(new Function))
-        expect('function',test(new Function()))
-        expect('function',test(function () {}))
-        expect('function',test(function a() {}))
+    it('Function', function () {
+        expect('function', test(new Function))
+        expect('function', test(new Function()))
+        expect('function', test(function () {
+        }))
+        expect('function', test(function a() {
+        }))
     })
-    it('Array',function () {
-        expect('array',test(new Array))
-        expect('array',test(new Array()))
-        expect('array',test([]))
-        expect('array',test([1,2,3]))
-        expect('array',test(['12','string','']))
-        expect('array',test(['']))
+    it('Array', function () {
+        expect('array', test(new Array))
+        expect('array', test(new Array()))
+        expect('array', test([]))
+        expect('array', test([1, 2, 3]))
+        expect('array', test(['12', 'string', '']))
+        expect('array', test(['']))
     })
-    it('Date',function () {
-        expect('date',test(new Date))
-        expect('date',test(new Date()))
+    it('Date', function () {
+        expect('date', test(new Date))
+        expect('date', test(new Date()))
     })
-    it('RegExp',function () {
-        expect('regexp',test(new RegExp))
-        expect('regexp',test(new RegExp()))
-        expect('regexp',test(/^/))
-        expect('regexp',test(/$/))
-        expect('regexp',test(/\w*/))
+    it('RegExp', function () {
+        expect('regexp', test(new RegExp))
+        expect('regexp', test(new RegExp()))
+        expect('regexp', test(/^/))
+        expect('regexp', test(/$/))
+        expect('regexp', test(/\w*/))
     })
-    it('Error',function () {
-        expect('error',test(new Error))
-        expect('error',test(new Error()))
+    it('Error', function () {
+        expect('error', test(new Error))
+        expect('error', test(new Error()))
     })
-    it('Object',function () {
-        expect('object',test(new Object))
-        expect('object',test(new Object()))
-        expect('object',test({}))
-        expect('object',test({a:1}))
+    it('Object', function () {
+        expect('object', test(new Object))
+        expect('object', test(new Object()))
+        expect('object', test({}))
+        expect('object', test({a: 1}))
     })
-    it('Undefined',function () {
-        expect('undefined',test(undefined))
+    it('Undefined', function () {
+        expect('undefined', test(undefined))
     })
-    it('Null',function () {
-        expect('null',test(null))
+    it('Null', function () {
+        expect('null', test(null))
     })
 })
+
+describe('测试"js/main.js"中 _toDefault 函数', function () {
+    var test = main._toDefault
+    it('Number', function () {
+        expect(-1, test(new Number))
+        expect(-1, test(new Number()))
+        expect(-1, test(0))
+        expect(-1, test(56))
+        expect(-1, test(-5656))
+        expect(-1, test(0.56))
+        expect(-1, test(5.56))
+    })
+    it('String', function () {
+        expect('', test(new String))
+        expect('', test(new String('fdfdfd')))
+        expect('', test(''))
+        expect('', test('xxx'))
+        expect('', test('122456'))
+        expect('', test('122xxx'))
+        expect('', test('xxx123456'))
+        expect('', test('*xxx1234fd56'))
+    })
+    it('Boolean', function () {
+        expect(false, test(false))
+        expect(false, test(true))
+    })
+    it('Undefined', function () {
+        expect('', test(undefined))
+    })
+    it('Null', function () {
+        expect('', test(null))
+    })
+})
+
+describe('测试"js/main.js"中 _baseEmpty2string 函数', function () {
+    var test = main._baseEmpty2string
+    it('null',function () {
+        expect('',test(null))
+    })
+    it('""',function () {
+        expect('',test(''))
+    })
+    it('undefined',function () {
+        expect('',test(undefined))
+    })
+})
+
+
